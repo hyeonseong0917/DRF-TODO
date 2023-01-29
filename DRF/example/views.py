e0bda86a5e07ad74f7e8fda025c8a7de9e4db499
hyeonseong0917 <sagiri0917@gmail.com>
2023-01-29 19:31:57 +0900

    DRF basic

---
+++ DRF/example/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("hello world!")
+
+
+# Create your views here.
